b368b23508e1add92ca77f9c7d90ef07fc241cc4
Tanya <gmztanya@gmail.com>
2024-10-14 22:15:14 +0530

    Added pagination and filter to the grid

--- railpass/src/components/home/home.tsx
+++ railpass/src/components/home/home.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the Data Grid
 import { stationsKerala } from '../../data/stations';
 import { Stations } from '../interfaces/stations';
+
 
 
 const Home = () => {
@@ -13,18 +14,30 @@ const Home = () => {
 
   // Column Definitions: Defines the columns to be displayed.
   const [colDefs] = useState<any>([
-    { field: "Name" },
-    { field: "Code" },
+    { field: "Name", filter: true, flex: 5 ,floatingFilter: true},
+    { field: "Code", filter: true,flex :5,floatingFilter: true },
   ]);
+  
+  const rowSelection = useMemo<any>(() => { 
+    return {
+        mode: 'singleRow',
+      };
+  }, []);
+
+  const pagination = true;
+const paginationPageSize = 20;
   
   return (
     <div
       className="ag-theme-quartz" // applying the Data Grid theme
-      style={{ height: 900, width: '100%' }} // the Data Grid will fill the size of the parent container
+      style={{ height: 900, width: '100%',textAlign:"center"}} // the Data Grid will fill the size of the parent container
     >
       <AgGridReact
         rowData={rowData}
+        pagination={pagination}
+        paginationPageSize={paginationPageSize}
         columnDefs={colDefs}
+        rowSelection={rowSelection}
       />
     </div>
   );
