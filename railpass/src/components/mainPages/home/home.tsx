d82683a632332e6551ebe34f5df982f473c22cd9
Tanya <gmztanya@gmail.com>
2024-10-16 22:14:04 +0530

    Added navigation to timing page on click of station.

--- railpass/src/components/mainPages/home/home.tsx
+++ railpass/src/components/mainPages/home/home.tsx
@@ -1,28 +1,37 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the Data Grid
 import { stationsKerala } from '../../../data/stations';
 import { Stations } from '../../../interfaces/stations';
 import Title from '../../reusableComponents/titleComponent';
+import { useNavigate } from 'react-router-dom';
 
 
 const Home = () => {
-
+  const navigate = useNavigate();
   // Row Data: The data to be displayed.
   const [rowData] = useState<Stations[]>(stationsKerala);
 
   // Column Definitions: Defines the columns to be displayed.
   const [colDefs] = useState<any>([
-    { field: "Name", filter: true, flex: 5 ,floatingFilter: true},
+    { field: "Name", filter: true, flex: 5 ,floatingFilter: true ,
+      cellRenderer: (params:any) => {
+      return (
+          <span
+              style={{ cursor: 'pointer', color: 'blue' }}
+              onClick={() => handleCellClick(params.value)}
+          >
+              {params.value}
+          </span>
+      );
+  }},
     { field: "Code", filter: true,flex :5,floatingFilter: true },
   ]);
-  
-  const rowSelection = useMemo<any>(() => { 
-    return {
-        mode: 'singleRow',
-      };
-  }, []);
+
+  const handleCellClick = (id:any) => {
+    navigate(`/timing/${id}`);
+};
 
   const pagination = true;
 const paginationPageSize = 20;
@@ -31,15 +40,14 @@ const paginationPageSize = 20;
     <>
     <Title title='Select the station nearest to your crossing' />
     <div
-      className="ag-theme-quartz" // applying the Data Grid theme
-      style={{ height: '100%', width: '100%',textAlign:"center"}} // the Data Grid will fill the size of the parent container
+      className="ag-theme-quartz"
+      style={{ height: '100%', width: '100%',textAlign:"center"}}
     >
       <AgGridReact
         rowData={rowData}
         pagination={pagination}
         paginationPageSize={paginationPageSize}
         columnDefs={colDefs}
-        rowSelection={rowSelection}
       />
     </div>
     </>
