73e6ab2013d6439589b61049cdf949a7af42ceb4
Tanya <gmztanya@gmail.com>
2024-10-31 21:49:03 +0530

    api changes

--- railpass/src/services/requests.ts
+++ railpass/src/services/requests.ts
@@ -1,16 +1,15 @@
-
+import { trainsBetweenStationsURL, apiKey } from "../config/url";
 export const getTrainsBetweenStations = async () =>{
-    const url = 'https://irctc1.p.rapidapi.com/api/v3/trainBetweenStations';
     const queryParams = new URLSearchParams({
       fromStationCode: 'BVI',
       toStationCode: 'NDLS',
       dateOfJourney: '2024-10-30'
     });
   
-    const response = await fetch(`${url}?${queryParams}`, {
+    const response = await fetch(`${trainsBetweenStationsURL}?${queryParams}`, {
       method: 'GET',
       headers: {
-        'X-RapidAPI-Key': '951fb57393msh3e064b5c35f3652p1bfbe6jsn1eeb065efa33',
+        'X-RapidAPI-Key': `${apiKey}`,
         'X-RapidAPI-Host': 'irctc1.p.rapidapi.com'
       }
     });
