8cc2918fb87787f0cfb9841c58e9bd83f5ccaeae
Tanya <gmztanya@gmail.com>
2024-11-05 18:49:50 +0530

    timings page aggrid added

--- railpass/src/components/mainPages/timing/timing.tsx
+++ railpass/src/components/mainPages/timing/timing.tsx
@@ -1,15 +1,86 @@
-import React from 'react'
-import { useParams, useLocation } from 'react-router-dom';
+import { AgGridReact } from "ag-grid-react";
+import React, { useState } from "react";
+import { useParams, useLocation } from "react-router-dom";
 
+const extractTrainData = (trainData: any) => {
+  const extractedTrainData = trainData.data.map(
+    (train: {
+      from_sta: any;
+      to_sta: any;
+      train_number: any;
+      train_name: any;
+      from_station_name: any;
+      to_station_name: any;
+    }) => ({
+      train_number: train.train_number,
+      train_name: train.train_name,
+      from_station: train.from_station_name,
+      to_station: train.to_station_name,
+      departure_time: train.from_sta,
+      arrival_time: train.to_sta,
+    })
+  );
+  return extractedTrainData;
+};
 
 const Timing = () => {
-  const { stationCode } = useParams();  
-  console.log(stationCode);  
+  // const { stationName } = useParams();
+
   const location = useLocation();
   const trainData = location.state?.data;
-console.log(trainData);
+  const extractedTrainData = extractTrainData(trainData);
+  const [rowData] = useState(extractedTrainData);
+  const [colDefs] = useState<any>([
+    {
+      field: "train_number",
+      headerName: "Train Number",
+      filter: true,
+      flex: 2,
+      floatingFilter: true,
+    },
+    {
+      field: "train_name",
+      headerName: "Train Name",
+      filter: true,
+      flex: 4,
+      floatingFilter: true,
+    },
+    {
+      field: "from_station",
+      headerName: "From Station",
+      filter: true,
+      flex: 3,
+      floatingFilter: true,
+    },
+    {
+      field: "to_station",
+      headerName: "To Station",
+      filter: true,
+      flex: 3,
+      floatingFilter: true,
+    },
+    {
+      field: "departure_time",
+      headerName: "Departure Time",
+      filter: true,
+      flex: 2,
+      floatingFilter: true,
+    },
+    {
+      field: "arrival_time",
+      headerName: "Arrival Time",
+      filter: true,
+      flex: 2,
+      floatingFilter: true,
+    },
+  ]);
   return (
-    <div>{stationCode}</div>
-  )
-}
+    <div
+      className="ag-theme-quartz"
+      style={{ height: "100%", width: "100%", textAlign: "center" }}
+    >
+      <AgGridReact rowData={rowData} pagination={false} columnDefs={colDefs} />
+    </div>
+  );
+};
 export default Timing;
