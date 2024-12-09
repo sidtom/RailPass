d370a663be37c95cc4be2e141a77a3989c7ef926
Tanya <gmztanya@gmail.com>
2024-12-09 22:52:56 +0530

    some css changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { Login } from "./components/mainPages/login";
 import { LiveStatus } from "./components/mainPages/liveStatus";
 import { Timing } from "./components/mainPages/timing";
 import { appContext } from "./context/context";
-import Container from "@mui/material/Container";
 
 function App() {
   const [stationCode, setStationCode] = useState("");
@@ -15,16 +14,14 @@ function App() {
     <appContext.Provider
       value={{ stationCode, setStationCode, timingData, setTimingData }}
     >
-      <Container maxWidth="xl" className="mainContainer">
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="liveStatus/:trainNo" element={<LiveStatus />} />
-            <Route path="home" element={<Home />} />
-            <Route path="timing/:stationName" element={<Timing />} />
-          </Routes>
-        </div>
-      </Container>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="liveStatus/:trainNo" element={<LiveStatus />} />
+          <Route path="home" element={<Home />} />
+          <Route path="timing/:stationName" element={<Timing />} />
+        </Routes>
+      </div>
     </appContext.Provider>
   );
 }
